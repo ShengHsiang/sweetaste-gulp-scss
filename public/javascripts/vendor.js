0e19d3ec55b9cad9cb7da67f6dffdfbdbc1dae9a
sheng <qzwxsa1234@gmail.com>
2018-10-06 12:55:57 +0800

    update responsive design and finished shop.html

--- public/javascripts/vendor.js
+++ public/javascripts/vendor.js
@@ -11,7 +11,13 @@
 
 // items heart selector event
 $(document).ready(function(){
-    $('.like').on('click',function(){
+    $('.like').on('click', function(e){
+        e.preventDefault();
         $(this).toggleClass('click');
+    })
+
+    $('.navbar-btn').on('click', function(e) {
+        e.preventDefault();
+        $('body').toggleClass('navbar-show');
     })
 })
